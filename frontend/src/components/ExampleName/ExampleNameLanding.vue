dcc4cfcd3987763d4d0ee0760a33ad0b0fd2131a
Zheheng Huang <zhhhuang@ucdavis.edu>
2024-06-28 01:49:27 -0700

    basic executable version

--- frontend/src/components/ExampleName/ExampleNameLanding.vue
+++ frontend/src/components/ExampleName/ExampleNameLanding.vue
@@ -1,17 +1,17 @@
 <!--
- Copyright 2024 huangzheheng
- 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- 
-     https://www.apache.org/licenses/LICENSE-2.0
- 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+Copyright 2024 huangzheheng
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 -->
 <template>
   <div class="extended-section">
@@ -87,7 +87,7 @@
             <ul>
               <li>
                 doi (e.g.
-                <router-link :to="`/exampleroute/doi/${encodeURIComponent('10.1016.j.algal.2019.101702')}`">10.1016.j.algal.2019.101702</router-link>
+                <router-link :to="`/exampleroute/doi/${encodeURIComponent('10.1016.j.ymben.2013.09.007')}`">10.1016.j.ymben.2013.09.007</router-link>
                 )
               </li>
               <li>
